7c9850c6b1286e2de750b8980897bb49107f51fe
ishakcoiff <xr1337.user@icloud.com>
2024-08-05 19:38:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+    const now = new Date();
+  
+    const date = now.toISOString().split('T')[0];
+  
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const time = `${hours}:${minutes}`;
+    
+    document.getElementById('date').value = date;
+    document.getElementById('time').value = time;
