559de2474de6a29a449c71c8c022f0bf9af9bbcb
ishakcoiff <xr1337.user@icloud.com>
2024-08-06 14:00:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-    const now = new Date();
-  
-    const date = now.toISOString().split('T')[0];
-  
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const time = `${hours}:${minutes}`;
-    
-    document.getElementById('date').value = date;
-    document.getElementById('time').value = time;
+  document.addEventListener('DOMContentLoaded', (event) => {
+    try {
+        const now = new Date();
+        const date = now.toISOString().split('T')[0];
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const time = `${hours}:${minutes}`;
+
+        document.getElementById('date').value = date;
+        document.getElementById('time').value = time;
+    } catch (error) {
+        console.error('Error initializing date and time:', error);
+    }
+});
